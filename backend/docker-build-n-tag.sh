1dca668c3e96f7fd8f2e1a9546ba14ad949c0565
hamsolo <root@hweeks.com>
2020-02-22 09:02:45 -0800

    ci: debian uses the other order for users, lol

--- backend/docker-build-n-tag.sh
+++ backend/docker-build-n-tag.sh
@@ -2,5 +2,5 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -t dcpm-be-ci --build-arg GIT_COMMIT=$(git rev-parse HEAD) .
 docker tag dcpm-be-ci hams/dcpm-be:$(git rev-parse HEAD)
 docker tag dcpm-be-ci hams/dcpm-be:latest
-docker push hams/dcpm-be:$(git rev-parse HEAD)
-docker push hams/dcpm-be:latest
+# docker push hams/dcpm-be:$(git rev-parse HEAD)
+# docker push hams/dcpm-be:latest
