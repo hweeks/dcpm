b6b5ca837ebac04f7f012536eb4e011f7b370274
hammy-late-nite <git@hweeks.com>
2021-12-16 08:37:38 -0800

    feat: and the fe

--- backend/lib/startup.ts
+++ backend/lib/startup.ts
@@ -2,7 +2,6 @@ import { gimme_dat_local_db_uri } from "./server/db-local";
 import { is_local_dev, is_test } from "./constants";
 
 const init_db = async () => {
-  console.log('yoooooo')
   if (is_test || is_local_dev) {
     await gimme_dat_local_db_uri()
   }
